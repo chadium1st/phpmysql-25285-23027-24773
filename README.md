44fffa675f3b90cf87248a4fbde181df8ca548a0
Ismail Ahmad <106441180+chadium1st@users.noreply.github.com>
2023-06-02 15:39:26 +0500

    Connected database.
    - data gets saved into a "$response" variable in the form of JSON objects.
    - added the files and folders required according to the documentation.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # web-programming-project-02
-
-This is a job posting platform where you can post jobs or get hired by recruiters. You either join as a recruiter or a seeker. you can search for jobs of your choice. This site doesn't charge you anything.
+Web Programming Final Project
--- db-connect.php
+++ db-connect.php
@@ -3,24 +3,45 @@
 $host = "localhost";
 $admin = "root";
 $password = "";
-$database = "houses";
+$database = "jobs"; 
 
 $conn = mysqli_connect($host, $admin, $password, $database);
 
 if(!$conn){
     die("Database Connection Error: " . mysqli_connect_error());
 }
+
+else if($conn->connect_error) {
+    trigger_error('Database connection failed: ' .$conn->connect_error, E_USER_ERROR);  
+}
+
 else {
-    $query = "SELECT * from house;";
+    // get the tables from the database
+
+    $query = "SHOW TABLES";
     $result = mysqli_query($conn, $query);
 
     // Prepare response array
     $response = array();
 
     if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            // Add each row to the response array
-            $response[] = $row;
+        while($table = mysqli_fetch_row($result)) {
+            $table_name = $table[0];
+            $table_data = array();
+
+            // selects all rows from the current table
+            $query_table = "SELECT * FROM $table_name";
+            $result_table = mysqli_query($conn, $query_table);
+
+            if(mysqli_num_rows($result_table) > 0) {
+                while($row = mysqli_fetch_assoc($result_table)) {
+                    // add each row to table's array
+                    $table_data[] = $row;
+                }
+            }
+
+            // add everything to the response array object
+            $response[$table_name] = $table_data;
         }
     }
 
@@ -29,5 +50,5 @@ else {
 
     // Output the response as JSON string
     echo json_encode($response);
-
+    $conn->close();
 }
