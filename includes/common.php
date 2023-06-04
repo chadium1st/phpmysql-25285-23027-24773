f515ce98ade7b9a2f7b59c32ae6c1d48d908ea2f
Abdullah Ali Dewan <abdulahdewan@gmail.com>
2023-06-04 16:03:33 +0500

    Add files via upload

--- includes/common.php
+++ includes/common.php
@@ -7,3 +7,17 @@ function sanitizeInput($data) {
     return $data;
 }
 
+function returnHome($type) {
+    if($type == "recruiter") {
+        header("Location: recruiter-home.php");
+        exit;
+    }
+    else if($type == "seeker") {
+        header("Location: seeker-home.php");
+        exit;
+    }
+    else if($type == "admin") {
+        header("Location: admin-home.php");
+        exit;
+    }
+}
---
+++ includes/recruiter-header.php
@@ -0,0 +1,35 @@
+        <?php include "includes/meta-tags.php" ?>
+        <div class="header">
+        <header class="homepage">
+        <input type="search" placeholder="Search here..." class="search">
+        <img class="logo" src="./graphics/logo.png" alt="logo of the website">   
+        <label>
+        
+                <input type="checkbox" class="input">
+                <div class="toggle">
+                <span class="top_line common"></span>
+                <span class="middle_line common"></span>
+                <span class="bottom_line common"></span>
+                </div>
+               
+        
+            <div class="menu">
+                <br><br>
+            <h1 class="h1">Menu</h1>
+            <nav>
+            <ul class="bar">
+            <li><a href="./index.html">Johannesburg</a></li>
+            <li><a href="./introtosouthafrica.html">Introduction to South Africa</a></li>
+            <li><a href="./history.html">History of Johannesburg</a></li>
+            <li><a href="./culture.html">Culture</a></li>
+            <li><a href="./lifeinjohannesburg.html">Life in Johannesburg</a></li>
+            <li><a href="./lawgovernment.html">Law & Government</a></li>
+            <li><a href="./placestovisit.html">Places To Visit</a></li>
+            <li><a href="./contactus.html">contact us</a></li>
+        </ul>
+    </nav>
+        </div>
+        </label>
+        <img class="profile" src="./graphics/profilepicture.png" alt="profilepicture"> 
+        </header>
+        </div>
