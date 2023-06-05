5c13c465b90f8e97685996ab9c95fedda0625816
KashanHumayun <103729683+KashanHumayun@users.noreply.github.com>
2023-06-06 00:43:55 +0500

    Update header.php according to the new 000.php

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,23 @@
-<?php 
-    include "login-action.php";
-?>
-
-<link rel="stylesheet" href="css/styles.css">
-<header>
-    <?php 
-        if($type == "recruiter") {?>
-        <nav>
-
-        </nav>
-    <?php }?>
-
-    <?php 
-        if($type == "seeker") {?>
-        <nav>
-
-        </nav>
-    <?php }?>
-
-    <?php 
-        if($type == "admin") {?>
-        <nav>
-
-        </nav>
-    <?php }?>
-</header>
+<header class="header">
+    <div class="toggle-btn" onclick="toggleSidebar()">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="logo">
+      <img src="graphic/logo.png" alt="Logo">
+    </div>
+    <div class="search">
+        <input type="text" placeholder="Search..">
+        <button type="submit">
+          <i class="fa fa-search">
+            <img class="search-icon" src="graphic/searchsvg.svg" alt="search icon" title="Search">
+          </i>
+        </button>
+    </div>
+    <nav class="nav">
+      <ul>
+        <li><a href="#"><img id="imagetoinvert" src="graphic/home_filled.svg" alt="home icon" title="Home"></a></li>
+      </ul>
+    </nav>
+  </header>
