d17f5d986e7df360828d72426d10a189833f2abd
KashanHumayun <103729683+KashanHumayun@users.noreply.github.com>
2023-06-06 00:45:18 +0500

    Add files via upload

---
+++ includes/aside.php
@@ -0,0 +1,9 @@
+<aside class="sidebar">
+    <div id="sidebarLogocontainer">
+      <p><b>JOB LICIOUS</b></p><a href="#">
+      <img id="logoImg" src="graphic/logo.png" alt="Logo"></a>
+    </div>
+    <?php
+      include "includes/navigation.php";
+    ?>
+  </aside>
