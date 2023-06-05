aec4393588c3c4fe1a4819ea6fea2c3d366612c1
KashanHumayun <103729683+KashanHumayun@users.noreply.github.com>
2023-06-06 01:08:05 +0500

    Update login.php updated the login.php with new layout

--- login.php
+++ login.php
@@ -7,10 +7,13 @@ session_start();
 <html>
 <head>
     <title>Login Page</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <?php
+        include "includes/metatags.php";
+    ?>
 </head>
 <body>
-    <div class="container">
+    <div id="supercontainer">
+    <div id="container">
         <h1>Login</h1>
         <?php
         // Check if there is a login error message
@@ -21,16 +24,17 @@ session_start();
         ?>
         <form action="login-action.php" method="POST">
             <label for="email">Email:</label>
-            <input type="text" name="email" required>
+            <input type="text" class="login-text"name="email" required>
 
             <label for="password">Password:</label>
-            <input type="password" name="password" required>
+            <input type="password" class="login-text" name="password" required>
 
             <input type="submit" value="Login">
         </form>
         <p>Don't have an account?
-        <a href="register.php?type=recruiter">Register</a> as a recruiter! </br>
-        <a href="register.php?type=seeker">Register</a> as a Job Seeker!</p>
+        <a href="register.php?type=recruiter" class="amper">Register</a> as a recruiter! </br>
+        <a href="register.php?type=job_seeker">Register</a> as a Job Seeker!</p>
+    </div>
     </div>
 </body>
 </html>
