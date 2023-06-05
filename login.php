0971761244e9fbd293c9841e29c61cf99cfe3885
KashanHumayun <103729683+KashanHumayun@users.noreply.github.com>
2023-06-06 01:09:58 +0500

    Update login.php added you in don't you have an account

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ session_start();
 
             <input type="submit" value="Login">
         </form>
-        <p>Don't have an account?
+        <p>Don't you have an account?
         <a href="register.php?type=recruiter" class="amper">Register</a> as a recruiter! </br>
         <a href="register.php?type=job_seeker">Register</a> as a Job Seeker!</p>
     </div>
